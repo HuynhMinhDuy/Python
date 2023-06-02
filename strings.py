5ebe72aeb18b801032c8c0dec97ea3629a456136
HuynhMinhDuy <79966404+HuynhMinhDuy@users.noreply.github.com>
2023-06-02 08:37:54 +0700

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,17 @@
+message = "wellcome to my product"
+
+print(message.title()) # output : Wellcome To My Product
+
+print(message.upper()) #output : WELLCOME TO MY PRODUCT
+
+message = "Wellcome To My Product"
+print(message.lower())  #output : wellcome to my product
+
+
+first_name = "ada"
+
+second_name = "lovelace"
+
+full_name = f"{first_name} {second_name}"
+
+print(full_name) #output : ada lovelace
